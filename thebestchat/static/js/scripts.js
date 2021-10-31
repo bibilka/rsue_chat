32abc7de6c909b09e7fbc63fed2446d688830187
Kamuzaka <kamuzaka01@gmail.com>
2021-10-31 14:08:07 +0300

    иконка профиля

--- thebestchat/static/js/scripts.js
+++ thebestchat/static/js/scripts.js
@@ -14,7 +14,7 @@ webSocket.onmessage = function(e) {
     chat.innerHTML +=
         '<div class="row mt-1">' +
         '<div class="col-3">' +
-        '<img src="https://avatars.mds.yandex.net/get-pdb/1873964/56d28094-012a-491a-96af-42e5aea26b2c/s1200?webp=false" class="rounded-circle img-fluid" alt="Аватар" height="30" width="30" style="float: right;">' +
+        '<img src="https://i.pinimg.com/originals/cc/6c/07/cc6c07880dfd0c337c875b3cdc6821c8.png" class="rounded-circle img-fluid" alt="Аватар" height="30" width="30" style="float: right;">' +
         '</div><div class="col-5"><p class="text-justify my-message" style="float: left">' +
         data.message +
         '</p></div></div>'
@@ -24,7 +24,7 @@ webSocket.onmessage = function(e) {
     chat.innerHTML +=
         '<div class="row mt-1 d-flex flex-row-reverse">' +
         '<div class="col-3">' +
-        '<img src="https://avatars.mds.yandex.net/get-pdb/1873964/56d28094-012a-491a-96af-42e5aea26b2c/s1200?webp=false" class="rounded-circle img-fluid" alt="Аватар" height="30" width="30" style="float: left;">' +
+        '<img src="https://i.pinimg.com/originals/cc/6c/07/cc6c07880dfd0c337c875b3cdc6821c8.png" class="rounded-circle img-fluid" alt="Аватар" height="30" width="30" style="float: left;">' +
         '</div><div class="col-5"><p class="text-justify friend-message" style="float: right">' +
         data.message +
         '</p></div></div>'
