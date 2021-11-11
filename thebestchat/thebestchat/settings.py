c8a93d4f5a94cdec3ba24b1bc94024fd0a083dc3
bibilka <lehavorotuncev@gmail.com>
2021-11-11 03:36:45 +0300

    test

--- thebestchat/thebestchat/settings.py
+++ thebestchat/thebestchat/settings.py
@@ -13,8 +13,7 @@ import os
 from pathlib import Path
 import django_heroku
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -169,3 +168,5 @@ CHANNEL_LAYERS = {
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Activate Django-Heroku.
+django_heroku.settings(locals())
