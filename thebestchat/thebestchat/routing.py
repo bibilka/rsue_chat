dbc01c062edf713398aceb3ea5007ba0207d4d51
bibilka <lehavorotuncev@gmail.com>
2021-11-11 16:17:55 +0300

    test

--- thebestchat/thebestchat/routing.py
+++ thebestchat/thebestchat/routing.py
@@ -5,7 +5,7 @@ import chat.routing as routing
 from django.core.asgi import get_asgi_application
 
 application = ProtocolTypeRouter({
-    # 'http': get_asgi_application(),
+    'http': get_asgi_application(),
     # (http->django views is added by default)
     'websocket': AuthMiddlewareStack(
         URLRouter(
