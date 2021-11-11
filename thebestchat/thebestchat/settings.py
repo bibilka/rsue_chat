5d6d499c95cbebbed018572addb062836baa5a4c
bibilka <lehavorotuncev@gmail.com>
2021-11-11 20:44:06 +0300

    remove redis from local development

--- thebestchat/thebestchat/settings.py
+++ thebestchat/thebestchat/settings.py
@@ -135,13 +135,18 @@ STATICFILES_DIRS = [
 ASGI_APPLICATION = "thebestchat.routing.application"
 
 CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
-        },
-    },
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+    }
 }
+# CHANNEL_LAYERS = {
+#     'default': {
+#         'BACKEND': 'channels_redis.core.RedisChannelLayer',
+#         'CONFIG': {
+#             "hosts": [('127.0.0.1', 6379)],
+#         },
+#     },
+# }
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
