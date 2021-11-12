741091274c95da158988d9f4c751a81d7e2b08c2
bibilka <lehavorotuncev@gmail.com>
2021-11-12 03:04:59 +0300

    routing fix

--- thebestchat/thebestchat/routing.py
+++ thebestchat/thebestchat/routing.py
@@ -1,12 +1,13 @@
 from .wsgi import *  # add this line to top of your code
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-import chat.routing as routing
+
 from django.core.asgi import get_asgi_application
 import os
 from pathlib import Path
 
 if (os.environ.get('RSUECHAT_PROD_ENV', 'False') == 'True'):
+    import thebestchat.chat.routing as routing
     application = ProtocolTypeRouter({
         'http': get_asgi_application(),
         'websocket': AuthMiddlewareStack(
@@ -16,6 +17,7 @@ if (os.environ.get('RSUECHAT_PROD_ENV', 'False') == 'True'):
         ),
     })
 else:
+    import chat.routing as routing
     application = ProtocolTypeRouter({
         'websocket': AuthMiddlewareStack(
             URLRouter(
