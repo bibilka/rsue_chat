da36401029c231d3117c4d52d4119507bb03786b
bibilka <lehavorotuncev@gmail.com>
2021-11-12 03:14:15 +0300

    routing ascgi

--- thebestchat/thebestchat/routing.py
+++ thebestchat/thebestchat/routing.py
@@ -1,27 +1,15 @@
 from .wsgi import *  # add this line to top of your code
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-
+import chat.routing as routing
 from django.core.asgi import get_asgi_application
-import os
-from pathlib import Path
 
-if (os.environ.get('RSUECHAT_PROD_ENV', 'False') == 'True'):
-    import thebestchat.chat.routing as routing
-    application = ProtocolTypeRouter({
-        'http': get_asgi_application(),
-        'websocket': AuthMiddlewareStack(
-            URLRouter(
-                routing.websocket_urlpatterns
-            )
-        ),
-    })
-else:
-    import chat.routing as routing
-    application = ProtocolTypeRouter({
-        'websocket': AuthMiddlewareStack(
-            URLRouter(
-                routing.websocket_urlpatterns
-            )
-        ),
-    })
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    # (http->django views is added by default)
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            routing.websocket_urlpatterns
+        )
+    ),
+})
